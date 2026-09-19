4511789c2f6d24964c349b09b340be192ce06585
Fatma_Mohamed <fm5252498@gmail.com>
2026-09-19 21:23:17 +0300

    Question01

---
+++ Smart Delivery Management System/DeliveryAddress.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smart_Delivery_Management_System
+{
+    internal struct DeliveryAddress
+    {
+
+        #region Question01
+        public string City;
+
+        public string Street;
+
+        public int BuildingNumber;
+
+
+        public DeliveryAddress(string city, string street, int buildingNumber)
+        {
+
+            this.City = city;
+
+            this.Street = street;
+
+            this.BuildingNumber = buildingNumber;
+
+        }
+
+        public string GetFullAddress()
+        {
+            return $" City: {City}        Street: {Street}         Building Number: {BuildingNumber} ";
+        }
+
+        #endregion
+    }
+}
---
+++ Smart Delivery Management System/Shipment.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smart_Delivery_Management_System
+{
+    internal class Shipment
+    {
+        
+        private string trackingCode;
+        private string description;
+        private decimal weight;
+        private decimal deliveryFee;
+
+
+        public DeliveryAddress Destination { get; set; }
+
+        public string TrackingCode
+        {
+            get => trackingCode;
+            private set
+            {
+
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    trackingCode = value;
+                }
+            }
+        }
+
+
+        public string Description
+        {
+            get => description;
+            set
+            {
+
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    description = value;
+                }
+            }
+        }
+
+
+        public decimal Weight
+        {
+            get => weight;
+            set
+            {
+
+                if (value > 0)
+                {
+                    weight = value;
+                }
+            }
+        }
+
+
+        public decimal DeliveryFee
+        {
+            get => deliveryFee;
+            private set
+            {
+
+                if (value > 0)
+                {
+                    deliveryFee = value;
+                }
+            }
+        }
+
+
+
+        public Shipment(string trackingCode)
+        {
+
+            this.trackingCode = "DEFAULT_CODE";
+            this.description = "Unknown";
+            this.weight = 1m;
+            this.deliveryFee = 50m;
+            this.Destination = default;
+
+
+            TrackingCode = trackingCode;
+        }
+
+
+        public Shipment(string trackingCode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination)
+        {
+
+            this.trackingCode = "DEFAULT_CODE";
+            this.description = "Unknown";
+            this.weight = 1m;
+            this.deliveryFee = 50m;
+            this.Destination = destination;
+
+
+            TrackingCode = trackingCode;
+            Description = description;
+            Weight = weight;
+            DeliveryFee = deliveryFee;
+        }
+
+
+        public void UpdateDeliveryFee(decimal newFee)
+        {
+            if (newFee > 0)
+            {
+                DeliveryFee = newFee;
+            }
+        }
+
+        #region Question01
+
+        public void UpdateWeight(decimal newWeight)
+        {
+            if (newWeight > 0)
+            {
+                Weight = newWeight;
+            }
+        }
+
+
+        public void UpdateWeight(decimal newWeight,decimal extraPackingWeight)
+        {
+            if (newWeight > 0&& extraPackingWeight>0)
+            {
+                Weight = newWeight + extraPackingWeight;
+            }
+        }
+
+
+        public virtual decimal EstimatedCost
+        {
+            get => DeliveryFee + ((decimal)Weight * 5m);
+        }
+
+
+        public virtual void PrintShipment()
+        {
+            Console.WriteLine("");
+            Console.WriteLine($" Tracking Code : {TrackingCode}");
+            Console.WriteLine($" Description   : {Description}");
+            Console.WriteLine($" Weight        : {Weight} kg");
+            Console.WriteLine($" Delivery Fee  : ${DeliveryFee}");
+            Console.WriteLine($" Estimated Cost: ${EstimatedCost}");
+            Console.WriteLine($" Destination   : {Destination.GetFullAddress()}");
+            Console.WriteLine("");
+        }
+
+
+        #endregion
+
+
+
+    }
+}
