e3e647e0400e331ccbb709d049790c7296c25ba2
Fatma_Mohamed <fm5252498@gmail.com>
2026-09-19 22:20:21 +0300

    Question06

---
+++ Smart Delivery Management System/DeliveryHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smart_Delivery_Management_System
+{
+    internal class DeliveryHelper
+    {
+
+        #region Question06
+
+        public static void PrintShipmentDetails(Shipment shipment)
+        {
+            if (shipment != null)
+            {
+               
+                shipment.PrintShipment();
+            }
+        }
+
+        #endregion
+    }
+}
