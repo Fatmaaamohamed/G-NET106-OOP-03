55e3d18fe3e77f25109111c68c8cff49e79eae58
Fatma_Mohamed <fm5252498@gmail.com>
2026-09-19 21:00:33 +0300

    Question01 Part01

--- Smart Delivery Management System/Program.cs
+++ Smart Delivery Management System/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            
+            #region part01
+
+            #region Question01
+
+            /* a) Method Overloading (Compile-Time): Multiple methods in the same class with the same name but different parameters (type, number, or order).
+                  Method Overriding (Run-Time): A subclass provides a specific implementation of a method already declared in its parent class using the exact 
+                  same signature (name, parameters, and return type).
+             
+             
+               b) Early binding is resolved at compile time by the compiler,bases the call on the variable's reference type,uses new or no keywords and applies to overloading,
+                  method hiding, static, and non-virtual methodsis faster due to fixed target resolution but offers less flexibility.
+                  
+                  late binding is resolved at run time by the CLR, relies on the actual object in memory , uses virtual and override keywords and applies to method overriding,
+                  abstract methods, and interface methods, is slightly slower due to runtime per-call lookups but enables dynamic polymorphism.
+             
+             
+             
+             */
+
+
+            #endregion
+
+
+
+
+            #endregion
+
         }
     }
 }
