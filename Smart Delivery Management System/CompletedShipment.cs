a96873294e2cb0e2c82e28b8ed5874a6e0cbb256
Fatma_Mohamed <fm5252498@gmail.com>
2026-09-19 22:25:03 +0300

    Question07

---
+++ Smart Delivery Management System/CompletedShipment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smart_Delivery_Management_System
+{
+    internal sealed class CompletedShipment : Shipment
+    {
+        #region Question07
+        public CompletedShipment(string trackingCode)
+            : base(trackingCode)
+        {
+        }
+
+        public CompletedShipment(string trackingCode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination)
+            : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+        }
+        #endregion
+    }
+}
